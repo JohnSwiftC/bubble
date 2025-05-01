eb38e975386c1c202f844d0b72a0f541f93032b6
JohnSwiftC <johnswift@live.com>
2025-04-30 19:24:01 -0500

    remove usestate from app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-import { useState } from 'react'
 import './App.css'
 
 import Quote from './comps/Quote'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <div className="w-screen h-screen bg-gradient-to-r from-red-500 to-orange-400 p-3">
+        <div className="text-center bg-gray-100 border border-gray-400 mb-3 p-3 rounded">
+          <p className="text-transparent bg-clip-text font-bold text-3xl bg-gradient-to-r from-red-500 to-orange-400">
+            bubble
+          </p>
+        </div>
         <div className="flex flex-wrap gap-3">
           <Quote text="I don't like people from Bushwick" name="John" likes={12000}/>
           <Quote text="I love Gus" name="John" likes={0}/>
