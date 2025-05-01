efbb351f53a107d0896cb111bbb51327e8b07d4b
JohnSwiftC <johnswift@live.com>
2025-04-30 20:00:18 -0500

    manually handle hover scaling for like animation later

--- src/comps/Quote.jsx
+++ src/comps/Quote.jsx
@@ -2,15 +2,23 @@ import { useState } from 'react'
 
 const Quote = (props) => {
     const [liked, setLiked] = useState(false);
+    const [scale, setScale] = useState(100);
 
     const onClick = () => {
         if (liked) setLiked(false);
         if (!liked) setLiked(true);
+
+        const timeout = setTimeout(() => {
+
+        });
     }
     
     return (
         <>
-            <div className={`bg-white p-3 rounded-md grow-1 text-center drop-shadow-sm border border-3 border-solid ${liked ? "border-purple-400" : "border-gray-400"} hover:scale-107 hover:z-100 transition`} onClick={onClick}>
+            <div className={`bg-white p-3 rounded-md grow-1 text-center drop-shadow-sm border border-3 border-solid ${liked ? "border-purple-400" : "border-gray-400"} hover:scale-${scale} hover:z-100 transition`} 
+                onClick={onClick}
+                onMouseEnter={() => setScale(107)}
+                onMouseLeave={() => setScale(100)}>
                 <span className="text-4xl text-black font-serif italic">"</span>
                 <span className="text-3xl text-black font-serif italic">{props.text}</span>
                 <span className="text-4xl text-black font-serif italic">"</span>
