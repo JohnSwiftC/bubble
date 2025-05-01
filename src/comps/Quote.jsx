d01dce75989eac67b8bccc2949ebb4070baa30da
JohnSwiftC <johnswift@live.com>
2025-04-30 19:40:50 -0500

    add liking logic to quote component

--- src/comps/Quote.jsx
+++ src/comps/Quote.jsx
@@ -1,8 +1,16 @@
+import { useState } from 'react'
+
 const Quote = (props) => {
+    const [liked, setLiked] = useState(false);
+
+    const onClick = () => {
+        if (liked) setLiked(false);
+        if (!liked) setLiked(true);
+    }
     
     return (
         <>
-            <div className="bg-white p-3 rounded grow-1 text-center drop-shadow-sm border border-solid border-gray-400 hover:scale-107 transition">
+            <div className={`bg-white p-3 rounded-md grow-1 text-center drop-shadow-sm border border-3 border-solid ${liked ? "border-purple-400" : "border-gray-400"} hover:scale-107 transition`} onClick={onClick}>
                 <span className="text-4xl text-black font-serif italic">"</span>
                 <span className="text-3xl text-black font-serif italic">{props.text}</span>
                 <span className="text-4xl text-black font-serif italic">"</span>
